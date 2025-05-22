02d9e43865f272a604abf10d3c529912b56e4416
bernal <bernal.lol110@gmail.com>
2025-05-22 11:02:20 -0500

    Mi primera chambaaa!

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,6 +1,27 @@
+import HeaderComponent from "../components/HeaderComponent"
+
 function BlogPage() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("El formulario se ha enviado")
+  }
   return (
-    <div>BlogPage</div>
+    <>
+      <HeaderComponent></HeaderComponent>
+      <div>BlogPage</div>
+        <h1>Fórmulario - Tú opinión</h1>
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <label htmlFor="userName">Nombre:</label>
+              <input type="text" id="userName" />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="comentario">Comentario:</label>
+              <input type="text" id="comentario" />
+            </fieldset>
+            <button>Enviar</button>
+          </form>
+    </>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import HeaderComponent from "../components/HeaderComponent"
 
 function HomePage() {
+  const saySomeThing = ()=>{
+    console.log("Di algo");
+    alert("GAAAAA");
+  }
   return (
     <>
       <HeaderComponent></HeaderComponent>
       <div>HomePage</div>
+      <hr/>
+      <button onClick={saySomeThing}>Pulsa Click</button>
     </>
     
   )
